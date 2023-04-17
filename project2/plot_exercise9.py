38a995216da8637399802e739ba46c63b546e323
Haoyu Guo <41633245+unclesamsCream@users.noreply.github.com>
2023-04-17 13:03:09 +0200

    plot pic

--- project2/plot_exercise9.py
+++ project2/plot_exercise9.py
@@ -25,13 +25,24 @@ with open('project2/experiment_result/max_error_2wise.txt', 'r') as f:
     for line in f:
         max_error_2wise.append(float(line.strip('\n')))
 
+
 tick = [3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 label = ['$2^3$','$2^4$','$2^5$','$2^6$','$2^7$','$2^8$','$2^9$','$2^{10}$',
             '$2^{11}$','$2^{12}$','$2^{13}$','$2^{14}$',
             '$2^{15}$','$2^{16}$','$2^{17}$','$2^{18}$',
             '$2^{19}$','$2^{20}$']
+
+plt.subplot(1,2,1)
 plt.xticks(tick, label)
-plt.plot(logr, average_error_2wise, label="Average relative error")
+plt.plot(logr, average_error_2wise,c = 'g', label="Average relative error")
+plt.legend(loc="upper left")
+plt.xlabel('Array size $r = 2^w$')
+plt.ylabel('Relative error')
+plt.savefig('ex9_pic.svg', format='svg')
+
+
+plt.subplot(1,2,2)
+plt.xticks(tick, label)
 plt.plot(logr, max_error_2wise, label="Max relative error")
 plt.legend(loc="upper left")
 plt.xlabel('Array size $r = 2^w$')
